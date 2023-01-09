4c816ec8381ba9cddc16e7dc4e7cce51d7ad76ae
EightFiveZeroEight <Business.8508@gmail.com>
2023-01-09 17:29:45 -0500

    Finalize User/Product associations

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,17 @@
 //this is the access point for all things database related!
 
 const db = require('./db')
+const Product = require('./models/Product')
 
 const User = require('./models/User')
 
 //associations could go here!
+User.hasMany(Product, { through: "cart" });
+Product.belongsToMany(User, { through: "cart" });
+
+// This will create a new model called cart, which will represent the join table for the many-to-many relationship between User and Product. Each row in the cart table will have a userId and a productId that link the user to the product they have added to their cart.
+
+// You can then use the addProduct and getProducts methods that Sequelize generates based on this association to add products to a user's cart and retrieve the products in a user's cart.
 
 module.exports = {
   db,
