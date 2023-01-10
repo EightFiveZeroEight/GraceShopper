b8ede32f2abd1efbbfa4c5f7359b42b95aaa2ac9
hchalin <hchalin98@gmail.com>
2023-01-10 14:36:15 -0700

    seeded user db w/ fake data

---
+++ seedData/users.js
@@ -0,0 +1,96 @@
+const userType = ['admin', 'user', 'guest']
+
+const userData = [
+  {
+    username: "hharkin0",
+    userType: userType[1],
+    password: "caZncbz0gz",
+    email: "gfairey0@forbes.com",
+  },
+  {
+    username: "ctrippitt1",
+    userType: 'guest',
+    password: "aI9yXYU",
+    email: "nwofenden1@amazonaws.com",
+  },
+  {
+    username: "trosedale2",
+    userType: 'admin',
+    password: "OufF6THG",
+    email: "ptrustie2@cafepress.com",
+  },
+  {
+    username: "dclamp3",
+    userType: 'guest',
+    password: "PCPLXsWJB3P",
+    email: "brumney3@sciencedaily.com",
+  },
+  {
+    username: "jpizzey4",
+    userType: 'guest',
+    password: "OL6L4NVeh",
+    email: "gwestcot4@google.ca",
+  },
+  {
+    username: "abarham5",
+    userType: 'guest',
+    password: "7b0hbMNaZn38",
+    email: "adyshart5@joomla.org",
+  },
+  {
+    username: "jlegassick6",
+    userType: 'guest',
+    password: "1VSwiS74GQ",
+    email: "kclaibourn6@addthis.com",
+  },
+  {
+    username: "afiddian7",
+    userType: 'guest',
+    password: "dC0NxbYr6f",
+    email: "mhaighton7@home.pl",
+  },
+  {
+    username: "ckees8",
+    userType: 'admin',
+    password: "jWdK8m",
+    email: "stordoff8@ft.com",
+  },
+  {
+    username: "sconers9",
+    userType: 'guest',
+    password: "kla9wbNyg",
+    email: "shuggan9@amazon.de",
+  },
+  {
+    username: "arentenbecka",
+    userType: 'user',
+    password: "wNPzXEv7J",
+    email: "areeda@hp.com",
+  },
+  {
+    username: "asaltmarshb",
+    userType: 'user',
+    password: "cEBE95lFTgM",
+    email: "lmacfaddenb@ibm.com",
+  },
+  {
+    username: "rsoulsbyc",
+    userType: 'guest',
+    password: "ypnihSSmNJo",
+    email: "elindegardc@ihg.com",
+  },
+  {
+    username: "tjerratschd",
+    userType: 'guest',
+    password: "HrPILCn",
+    email: "bcornickd@cmu.edu",
+  },
+  {
+    username: "vcustancee",
+    userType: 'user',
+    password: "AHWH47s",
+    email: "gwoodsforde@usatoday.com",
+  },
+];
+
+module.exports = userData
