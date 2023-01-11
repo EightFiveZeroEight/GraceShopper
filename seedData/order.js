aab95522cc0a06dd5324d475e7a387da40744566
hchalin <hchalin98@gmail.com>
2023-01-11 13:55:41 -0700

    added foriegn keys to seed fake data

--- seedData/order.js
+++ seedData/order.js
@@ -1,24 +1,28 @@
 const orderData = [
   {
-    invoice: '1',
-    total: '23.54',
-    date: new Date()
+    invoice: "1",
+    total: "23.54",
+    date: new Date(),
+    user_id: 2, // foriegn key
   },
   {
-    invoice: '2',
-    total: '23.54',
-    date: new Date()
+    invoice: "2",
+    total: "23.54",
+    date: new Date(),
+    user_id: 2,
   },
   {
-    invoice: '3',
-    total: '23.54',
-    date: new Date()
+    invoice: "3",
+    total: "23.54",
+    date: new Date(),
+    user_id: 1
   },
   {
-    invoice: '4',
-    total: '23.54',
-    date: new Date()
+    invoice: "4",
+    total: "23.54",
+    date: new Date(),
+    user_id: 3
   },
-]
+];
 
-module.exports = orderData
+module.exports = orderData;
