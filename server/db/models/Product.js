191c1de5c00d45c020b0c4fc3917b578016fb2f8
EightFiveZeroEight <Business.8508@gmail.com>
2023-01-12 14:40:51 -0500

    Deploy Sequelize Hook To Products.js 'inStock' quality; if >0 = true

--- server/db/models/Product.js
+++ server/db/models/Product.js
@@ -1,28 +1,42 @@
 const Sequelize = require("sequelize");
+const { LoaderTargetPlugin } = require("webpack");
 const db = require("../db");
 
-const Product = db.define("product", {
-  // One user  ; many products
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
+const Product = db.define(
+  "product",
+  {
+    // One user  ; many products
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: Sequelize.FLOAT,
+      allowNull: false,
+    },
+    category: {
+      type: Sequelize.ENUM({ values: ["food", "clothing", "electronics"] }),
+      allowNull: false,
+    },
+    quantity: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    inStock: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
   },
-  price: {
-    type: Sequelize.FLOAT,
-    allowNull: false,
-  },
-  category: {
-    type: Sequelize.ENUM({ values: ["food", "clothing", "electronics"] }),
-    allowNull: false,
-  },
-  inStock: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  quantity: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-});
+  {
+    hooks: {
+      beforeValidate: (product) => {
+        if (product.quantity != 0) {
+          product.inStock = true;
+        }
+      },
+    },
+  }
+);
 
 module.exports = Product;
